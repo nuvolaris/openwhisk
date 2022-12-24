366a8c780493ba835bcf7dea8b5f98c3847fc547
Michele Sciabarra <michele@sciabarra.com>
2022-12-24 12:12:55 +0000

    another revert

--- tools/travis/setupSystem.sh
+++ tools/travis/setupSystem.sh
@@ -27,7 +27,6 @@ RUNTIMES_MANIFEST=${1:-"/ansible/files/runtimes.json"}
 # This is required because it is timed out to pull the image during the test.
 docker pull openwhisk/example
 
-#ROOTDIR=$PWD/openwhisk
 cd $ROOTDIR/ansible
 
 $ANSIBLE_CMD openwhisk.yml -e manifest_file="$RUNTIMES_MANIFEST" -e db_activation_backend=ElasticSearch
